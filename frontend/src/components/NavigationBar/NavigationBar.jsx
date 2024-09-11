6620c6dd2c32a813d6065438fe39c2e3689180a4
rytiscer <rytis.cerniauskas@gmail.com>
2024-09-11 22:18:49 +0300

    ADded code to navigationBar component

--- frontend/src/components/NavigationBar/NavigationBar.jsx
+++ frontend/src/components/NavigationBar/NavigationBar.jsx
@@ -1,5 +1,23 @@
+import styles from "./NavigationBar.module.scss";
+
 const NavigationBar = () => {
-  return <div>NavigationBar</div>;
+  return (
+    <nav className={styles.navbar}>
+      <div className={styles.leftSection}>
+        <div className={styles.logo}>
+          <img src="/logo.png" alt="Logo" />
+        </div>
+        <div className={styles.navLinks}>
+          <button className={styles.navButton}>Home</button>
+          <button className={styles.navButton}>Services</button>
+          <button className={styles.navButton}>About You</button>
+        </div>
+      </div>
+      <div className={styles.rightSection}>
+        <button className={styles.loginButton}>Login/Register</button>
+      </div>
+    </nav>
+  );
 };
 
 export default NavigationBar;
