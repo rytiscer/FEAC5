de2ed7c53b43b9b0a593a260200406d95920c6cd
rytiscer <rytis.cerniauskas@gmail.com>
2024-09-18 18:53:18 +0300

    Added routes and pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,22 @@
+import { Route, Routes } from "react-router-dom";
 import NavigationBar from "./components/NavigationBar/NavigationBar";
-import Hero from "./components/Hero/Hero";
+
+import Home from "./pages/Home";
+import Services from "./pages/Services";
+import AboutUs from "./pages/AboutUs";
+import Login from "./pages/Login";
+import { ROUTES } from "./router/consts";
 
 const App = () => {
   return (
     <>
       <NavigationBar />
-      <Hero />
+      <Routes>
+        <Route path={ROUTES.HOME} element={<Home />} />
+        <Route path={ROUTES.SERVICES} element={<Services />} />
+        <Route path={ROUTES.ABOUT_US} element={<AboutUs />} />
+        <Route path={ROUTES.LOGIN} element={<Login />} />
+      </Routes>
     </>
   );
 };
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { BrowserRouter as Router } from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+ReactDOM.render(
+  <Router>
     <App />
-  </StrictMode>,
-)
+  </Router>,
+  document.getElementById("root")
+);
