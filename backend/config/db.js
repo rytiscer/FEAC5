91cef8e580012a19e972f1f949164cb6018f349f
rytiscer <rytis.cerniauskas@gmail.com>
2024-09-23 21:37:32 +0300

    Changed dbName in db.js file

--- backend/config/db.js
+++ backend/config/db.js
@@ -6,9 +6,7 @@ dotenv.config();
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI, {
-      dbName: "-", // name of DB
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
+      dbName: "homeService",
     });
     console.log("Connected to MongoDB with Mongoose");
   } catch (err) {
